87532fbdf3a9ef025fd3acfaf536f67de18b6bc1
maktezi <mlcnierras@gmail.com>
2024-08-13 12:10:00 +0800

    fix: routes and controller

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,8 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
-use App\Models\Leave;
-use App\Models\Appearance;
 
 class AdminController extends Controller
 {
@@ -17,9 +15,7 @@ class AdminController extends Controller
     public function dashboard()
     {
         $totalUser = User::count();
-        $totalLeave = Leave::count();
-        $totalAppearance = Appearance::count();
-        return view('admin.dashboard',compact('totalLeave','totalAppearance', 'totalUser'));
+        return view('admin.dashboard',compact('totalUser'));
     }
 
 }
